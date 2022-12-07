7de3099db6a14e08326f6b77f473349498036a5a
Horhik <horhik@disroot.org>
2022-12-07 06:59:46 +0300

    fix css to work with several themes

--- assets/css.ts
+++ assets/css.ts
@@ -1,5 +1,18 @@
 export const css = `
+
+
+
 <style>
+
+:root {
+   --color-green: #44CF6E;
+   --color-red: #863737;
+   --headers:#c14343;
+   --accent2: #863737;
+   --accent: #771f1f;
+   --background-primary: #1a1e24;
+   --tag-background: var(--tag);
+}
 .stats_wrap {
     width: 100%;
     display: flex;
@@ -107,7 +120,8 @@ align-content: center;
     background: var(--accent2);
     font-family: var(--font);
     padding: 9px 18px;
-    margin: 12px;
+    margin-bottom: 20px !important;
+
     border-radius: 16px;
 }
 
@@ -130,6 +144,7 @@ align-content: center;
     padding: 20px;
     font-family: var(--font);
     font-size: 1.9em;
+    margin: 15px;
     font-weight: bold;
     padding: 8px 16px;
 
@@ -169,7 +184,9 @@ align-content: center;
 .skills-item {
     display: flex;
     font-family: var(--font);
-    margin-bottom: 20px;
+    margin-bottom: 1.3em !important;
+    margin-right: 30px;
+
 }
 
 .skills-list{
@@ -178,6 +195,7 @@ align-content: center;
     margin: 0;
     display: flex;
     flex-wrap: wrap;
+    justify-content: space-around;
 }
 
 .skills-item::before {
@@ -191,23 +209,25 @@ align-content: center;
     background-color: var(--tag-background);
     padding: 5px;
     border-radius: 10px;
-    width: 7em;
+    width: 100%;
+
 }
 
 .skills-item-value {
 
-    font-size: 1.7em;
+    font-size: 1.3em;
     font-weight: bold;
     z-index: 3;
     background-color: var(--accent);
     float: left;
-    transform: scale(1.4);
+    transform: scale(1.4) translateX(-3px);
+
     display: flex;
     align-items: center;
     text-align: center;
     align-content: center;
     justify-content: center;
-    width: 1.4em;
+    width: 1.8em;
     height: 1.4em;
     border-radius: 10px;
 
@@ -215,14 +235,14 @@ align-content: center;
 
 
 .skills-item-label {
-    font-size: 1.8em;
+    font-size: 1.4em;
     background-color: var(--accent2);
     padding: 5px;
     border-radius: 10px;
     display: flex;
     align-items: center;
-    width: 7em;
-    padding-left: 0.5em;
+    width: 100%;
+    padding: 10px;
     text-align: center;
 }
 /*
@@ -232,9 +252,9 @@ align-content: center;
 */
 
 .skill-item-mark {
-    font-size: 2em;
+    font-size: 1em;
     font-weight: bold;
-    margin-left: 25px;
+    margin-left: 16px;
     border-radius: 20px;
     margin-top: 6px;
 }
